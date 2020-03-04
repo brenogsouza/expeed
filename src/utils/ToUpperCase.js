11510782af04dc02597bb96c33a9d118a6a6aa78
willian.rochagit config --global user.name <willian.rocha@viaflow.com.br>
2020-03-04 17:52:20 -0300

    final

--- src/utils/ToUpperCase.js
+++ src/utils/ToUpperCase.js
@@ -2,6 +2,9 @@ function ToUpperCase(fileName) {
     const fileNameArray = [...fileName];
     fileNameArray.splice(0, 1, fileNameArray[0].toUpperCase());
 
+    if (fileNameArray[-1] === 's')
+        return fileNameArray.pop().join(''); 
+
     return fileNameArray.join('');
 }
 
